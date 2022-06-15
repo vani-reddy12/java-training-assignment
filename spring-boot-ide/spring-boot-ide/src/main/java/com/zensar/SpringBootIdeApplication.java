0f08cbc1845c97466cc9dd440a385645f93e55e4
vani-reddy12 <105474273+vani-reddy12@users.noreply.github.com>
2022-06-15 23:50:35 +0530

    Add files via upload

---
+++ spring-boot-ide/spring-boot-ide/src/main/java/com/zensar/SpringBootIdeApplication.java
@@ -0,0 +1,13 @@
+package com.zensar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIdeApplication.class, args);
+	}
+
+}
