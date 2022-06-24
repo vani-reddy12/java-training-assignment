0cb5fc8725358b666a4ecc1fc71d0f7a400894ae
vani-reddy12 <105474273+vani-reddy12@users.noreply.github.com>
2022-06-24 08:59:54 +0530

    updated code of 22 nd and 23 rd june 2022

---
+++ spring boot coupo application 22,23/src/main/java/com/zensar/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.zensar.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.zensar.entity.ErrorResponse;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(value = NoSuchCouponExistsException.class )
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public @ResponseBody ErrorResponse handleException(NoSuchCouponExistsException ex) {
+		return new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
+	}
+
+	@ExceptionHandler(value =  CouponAlreadyExistsException.class)
+	@ResponseStatus
+	public @ResponseBody ErrorResponse handleException(CouponAlreadyExistsException ex) {
+		return new ErrorResponse(HttpStatus.CONFLICT.value(), ex.getMessage());
+	}
+}
